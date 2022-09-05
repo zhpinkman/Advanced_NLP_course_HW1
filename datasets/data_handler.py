5b24245d621530950aa0002e6a8cd402e804e3fe
div22loaner <div22loaner@D22ML-LOANER3.local>
2022-09-04 18:57:15 -0700

    Add the script to train and classify as well as split the give train to train and test

---
+++ datasets/data_handler.py
@@ -0,0 +1,37 @@
+from operator import methodcaller
+import pandas as pd
+import os
+
+from sklearn.model_selection import train_test_split
+
+
+def read_file(data_file: str):
+
+    with open(data_file) as file:
+        data = file.read().splitlines()
+
+        data_split = map(methodcaller("rsplit", "\t", 1), data)
+        texts, labels = map(list, zip(*data_split))
+    return texts, labels
+
+
+if __name__ == "__main__":
+    datasets = [file for file in os.listdir('.') if file.endswith('.txt')]
+
+    for dataset in datasets:
+        texts, labels = read_file(dataset)
+        X_train, X_test, y_train, y_test = train_test_split(
+            texts,
+            labels,
+            test_size=0.2,
+            random_state=42,
+            stratify=labels
+        )
+
+        with open(os.path.join('custom', f"{dataset}.train"), 'w') as f:
+            for text, label in zip(X_train, y_train):
+                f.write(f"{text}\t{label}\n")
+
+        with open(os.path.join('custom', f"{dataset}.test"), 'w') as f:
+            for text, label in zip(X_test, y_test):
+                f.write(f"{text}\t{label}\n")
--- train_eval.sh
+++ train_eval.sh
@@ -1,10 +1,19 @@
+for DATASET in "products" "questions" "4dim" "odiya"
+do
 python3 train.py \
     -m naivebayes \
-    -i datasets/products.train.txt \
-    -o nb.products.model
-
+    -i "datasets/custom/$DATASET.train.txt.train" \
+    -o "nb.$DATASET.model"
 
 python3 classify.py \
-    -m nb.products.model \
-    -i datasets/products.train.txt \
-    -o results/nb.products.train.preds
+    -m "nb.$DATASET.model" \
+    -i "datasets/custom/$DATASET.train.txt.train" \
+    -o "results/nb.$DATASET.train.preds"
+
+python3 classify.py \
+    -m "nb.$DATASET.model" \
+    -i "datasets/custom/$DATASET.train.txt.test" \
+    -o "results/nb.$DATASET.train.preds"
+
+done
+
