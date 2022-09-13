51641872b9d138a37644899acbf31da0cf0ef633
div22loaner <div22loaner@D22ML-LOANER3.local>
2022-09-12 17:49:45 -0700

    decrease the num epochs for perceptron

--- train_eval.sh
+++ train_eval.sh
@@ -3,14 +3,14 @@ wandb="bow_ngram_$ngram"
 features="bow"
 
 
-OUTPUT_FILE="results/stats_logistic_regression_$wandb.txt"
-rm -rf $OUTPUT_FILE 
-touch $OUTPUT_FILE
+# OUTPUT_FILE="results/stats_logistic_regression_$wandb.txt"
+# rm -rf $OUTPUT_FILE 
+# touch $OUTPUT_FILE
 
-for DATASET in "products" "questions" "4dim" "odiya"
-do
+# for DATASET in "products" "questions" "4dim" "odiya"
+# do
 
-echo "RESULTS FOR: $DATASET LOGISTIC REGRESSION" >> $OUTPUT_FILE
+# echo "RESULTS FOR: $DATASET LOGISTIC REGRESSION" >> $OUTPUT_FILE
 # python3 train.py \
 #     -m logistic_regression \
 #     -i "datasets/custom/$DATASET.train.txt.train" \
@@ -22,43 +22,43 @@ echo "RESULTS FOR: $DATASET LOGISTIC REGRESSION" >> $OUTPUT_FILE
 #     --wandb $wandb
 
 
-echo "TEST" >> $OUTPUT_FILE
-python3 classify.py \
-    -m "lr.$DATASET.model" \
-    -i "datasets/custom/$DATASET.train.txt.test" \
-    -o "results/lr.$DATASET.test.preds" \
+# echo "TEST" >> $OUTPUT_FILE
+# python3 classify.py \
+#     -m "lr.$DATASET.model" \
+#     -i "datasets/custom/$DATASET.train.txt.test" \
+#     -o "results/lr.$DATASET.test.preds" \
 
-python3 eval.py \
-    -t "datasets/custom/$DATASET.train.txt.true" \
-    -p "results/lr.$DATASET.test.preds" \
-    >> $OUTPUT_FILE
-done
+# python3 eval.py \
+#     -t "datasets/custom/$DATASET.train.txt.true" \
+#     -p "results/lr.$DATASET.test.preds" \
+#     >> $OUTPUT_FILE
+# done
 
-OUTPUT_FILE="results/stats_naive_bayes_$wandb.txt"
-rm -rf $OUTPUT_FILE 
-touch $OUTPUT_FILE
+# OUTPUT_FILE="results/stats_naive_bayes_$wandb.txt"
+# rm -rf $OUTPUT_FILE 
+# touch $OUTPUT_FILE
 
-for DATASET in "products" "questions" "4dim" "odiya"
-do
+# for DATASET in "products" "questions" "4dim" "odiya"
+# do
 
-echo "RESULTS FOR: $DATASET NAIVE BAYES" >> $OUTPUT_FILE
+# echo "RESULTS FOR: $DATASET NAIVE BAYES" >> $OUTPUT_FILE
 # python3 train.py \
 #     -m naivebayes \
 #     -i "datasets/custom/$DATASET.train.txt.train" \
 #     -o "nb.$DATASET.model"
 
-echo "TEST" >> $OUTPUT_FILE
-python3 classify.py \
-    -m "nb.$DATASET.model" \
-    -i "datasets/custom/$DATASET.train.txt.test" \
-    -o "results/nb.$DATASET.test.preds"
+# echo "TEST" >> $OUTPUT_FILE
+# python3 classify.py \
+#     -m "nb.$DATASET.model" \
+#     -i "datasets/custom/$DATASET.train.txt.test" \
+#     -o "results/nb.$DATASET.test.preds"
 
-python3 eval.py \
-    -t "datasets/custom/$DATASET.train.txt.true" \
-    -p "results/nb.$DATASET.test.preds" \
-    >> $OUTPUT_FILE
+# python3 eval.py \
+#     -t "datasets/custom/$DATASET.train.txt.true" \
+#     -p "results/nb.$DATASET.test.preds" \
+#     >> $OUTPUT_FILE
 
-done
+# done
 
 OUTPUT_FILE="results/stats_perceptron_$wandb.txt"
 rm -rf $OUTPUT_FILE 
@@ -68,15 +68,15 @@ for DATASET in "products" "questions" "4dim" "odiya"
 do
 
 echo "RESULTS FOR: $DATASET PERCEPTRON" >> $OUTPUT_FILE
-# python3 train.py \
-#     -m perceptron \
-#     -i "datasets/custom/$DATASET.train.txt.train" \
-#     -o "perceptron.$DATASET.model" \
-#     --dev "datasets/custom/$DATASET.train.txt.test" \
-#     --devlabels "datasets/custom/$DATASET.train.txt.true" \
-#     --epochs 30 \
-#     --features $features \
-#     --wandb $wandb
+python3 train.py \
+    -m perceptron \
+    -i "datasets/custom/$DATASET.train.txt.train" \
+    -o "perceptron.$DATASET.model" \
+    --dev "datasets/custom/$DATASET.train.txt.test" \
+    --devlabels "datasets/custom/$DATASET.train.txt.true" \
+    --epochs 12 \
+    --features $features \
+    --wandb $wandb
 
 
 echo "TEST" >> $OUTPUT_FILE
